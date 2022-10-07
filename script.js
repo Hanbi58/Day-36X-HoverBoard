04cbbd16f42f4e22cdf50400c1d9f675f7f9916c
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-06 17:51:03 -0700

    a better version

--- script.js
+++ script.js
@@ -25,30 +25,30 @@ function getColor() {
 }
 
 const container = document.querySelector(".container");
-// const root = document.documentElement;
 
 for (let i = 0; i < 126; i++) {
   const box = document.createElement("div");
   box.classList.add("box");
-  //   box.onmouseover = setColor;
-  //   box.innerHTML = '<div class="box" onmouseover = setColor()></div>';
-
   container.appendChild(box);
 }
 
 const boxes = document.querySelectorAll(".box");
 
+// boxes.forEach((box) => {
+//   box.addEventListener("mouseenter", () => {
+//     const color = getColor();
+//     box.style.backgroundColor = color;
+//     box.style.boxShadow = `0 0 2px 2px ${color}`;
+//   });
+//   box.addEventListener("mouseleave", () => {
+//     box.style.backgroundColor = "rgb(31, 31, 31)";
+//     box.style.boxShadow = "none";
+//   });
+// });
+const root = document.documentElement;
 boxes.forEach((box) => {
   box.addEventListener("mouseenter", () => {
     const color = getColor();
-    // box.style.backgroundColor = "none";
-    // box.style.transition = "none";
-    box.style.backgroundColor = color;
-    box.style.boxShadow = `0 0 2px 2px ${color}`;
-  });
-  box.addEventListener("mouseleave", () => {
-    // box.style.transition = "all 1s ease-in-out";
-    box.style.backgroundColor = "rgb(31, 31, 31)";
-    box.style.boxShadow = "none";
+    root.style.setProperty("--randomco", color);
   });
 });
